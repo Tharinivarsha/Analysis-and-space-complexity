6b529184415f4e829b32c5e2e944300e82dc8c4f
Tharinivarsha <chitraswamy11@gmail.com>
2024-10-02 17:43:20 +0530

    Analysis and space complexity

---
+++ main.py
@@ -0,0 +1,11 @@
+#Activity2
+# def sum(n):
+#     return n*(n+1)/2
+# print(sum(10))
+def array(n):
+    sum=0
+    for i in n:
+        sum=sum+i
+    return (sum)
+n=[12,2,3,1,45,32,67,93,42,16,56]
+print(array(n))
